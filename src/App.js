dddda01ca4dbc8c03eeeefaa33eb9a6f98160cf9
Milan Milenkovic <milenkovicmilan10@gmail.com>
2020-08-07 22:09:23 +0200

    :rocket:

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import db from "./firebase";
 import {
-  FormControl,
-  Input,
   Button,
   Paper,
   Typography,
   Slider,
   Divider,
   TextareaAutosize,
+  Modal,
 } from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -28,6 +27,16 @@ function App() {
   const [ideaObj, setIdeasObj] = useState({});
   const [ideaId, setIdeaId] = useState("");
   const [rating, setRating] = useState(3);
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([
+    "Personal life",
+    "Work",
+    "Fun",
+    "Travel",
+    "Education",
+    "Other",
+  ]);
+  const [category, setCategory] = useState([])
 
   const newtime = new Date().toLocaleString();
   // const time = firebase.database.ServerValue.TIMESTAMP
@@ -67,6 +76,7 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
     editOrDelete(values);
+    setRating(3);
   };
 
   const editOrDelete = (obj) => {
@@ -112,6 +122,26 @@ function App() {
     setRating(newValue);
   }
 
+  function handleEditCategory() {
+    setOpen(true);
+  }
+
+  function handleAddCategory() {
+    options.push(category)
+    setOpen(false)
+  }
+
+
+
+  function handleRemoveCategory(string){
+    options.splice(string, 1)
+    setOpen(false)
+  }
+
+  function handleCategoryInput(e){
+    setCategory(e.target.value)
+  }
+
   return (
     <div className="App">
       <Typography
@@ -132,32 +162,41 @@ function App() {
             display: "flex",
             flexDirection: "column",
             width: "400px",
-            height:"auto",
+            height: "auto",
             margin: "0 auto",
           }}
           onSubmit={handleSubmit}
         >
+          <label style={{ textAlign: "left", fontStyle: "italic" }}>
+            Title
+          </label>
           <input
             value={values.title}
             name="title"
             onChange={handleInput}
-            placeholder="idea name"
-            style={{marginBottom:".3rem", padding:"1rem", borderRadius:'5px', border:"1px solid lightblue", outlineColor:"blue"}}
+            style={{
+              marginBottom: ".3rem",
+              padding: "1rem",
+              borderRadius: "5px",
+              border: "1px solid lightblue",
+              outlineColor: "blue",
+            }}
           />
+          <label style={{ textAlign: "left", fontStyle: "italic" }}>
+            Description
+          </label>
           <TextareaAutosize
-          rowsMax={10}
+            rowsMax={10}
             name="longTitle"
             value={values.longTitle}
             onChange={handleInput}
-            placeholder="idea description"
-            style={{marginBottom:".3rem", padding:"1rem", borderRadius:'5px', border:"1px solid lightblue", outlineColor:"blue"}}
-          />
-          <input
-            name="expectation"
-            value={values.expectation}
-            onChange={handleInput}
-            placeholder="idea expectations"
-            style={{marginBottom:".3rem", padding:"1rem", borderRadius:'5px', border:"1px solid lightblue", outlineColor:"blue"}}
+            style={{
+              marginBottom: ".3rem",
+              padding: "1rem",
+              borderRadius: "5px",
+              border: "1px solid lightblue",
+              outlineColor: "blue",
+            }}
           />
 
           <Typography
@@ -165,8 +204,9 @@ function App() {
               display: "flex",
               justifyContent: "flex-start",
               fontStyle: "italic",
-              fontSize: "12px",
+              fontSize: "15px",
               marginTop: "1rem",
+              color:"black"
             }}
           >
             Rating
@@ -182,48 +222,111 @@ function App() {
             onChange={handleSlider}
             value={rating}
           />
-          <div
+          <label style={{ textAlign: "left", fontStyle: "italic" }}>
+            Category
+          </label>
+          <select
             value={values.category}
             name="category"
             onChange={handleInput}
-            style={{marginBottom:".3rem", padding:"1rem", borderRadius:'5px', border:"1px solid lightblue", outlineColor:"blue"}}
-            
+            style={{
+              marginBottom: ".3rem",
+              padding: "1rem",
+              borderRadius: "5px",
+              border: "1px solid lightblue",
+              outlineColor: "blue",
+            }}
           >
-            <button>Personal life</button>
-            <button>Fun</button>
-            <button>Work</button>
-            <button>Travel</button>
-            <button>Other</button>
-          </div>
-          <Button type="submit" variant='contained' color='primary'>{ideaId === "" ? "SAVE" : "Update"}</Button>
+            {options.map((option, index) => (
+              <option key={index}>{option}</option>
+            ))}
+          </select>
+
+          <Modal open={open} onClose={(e) => setOpen(false)}>
+            <Paper
+              style={{
+                width: "500px",
+                outlineColor:"blue",
+                display: "flex",
+                justifyContent: "center",
+                margin: "3rem auto",
+                flexDirection:"column",
+                padding:"1rem"
+              }}
+            >
+              {options.map((option, index)=>(
+                <div style={{display:"flex", justifyContent:"space-between"}}>
+                  <Typography key={index}>{option}</Typography>
+                <Button onClick={()=>handleRemoveCategory(index)}><DeleteIcon color='secondary'/></Button>
+
+                </div>
+              ))}
+              <input placeholder='add category' onChange={handleCategoryInput} style={{marginBottom: ".3rem",
+              padding: "1rem",
+              borderRadius: "5px",
+              border: "1px solid lightblue",
+              outlineColor: "blue"}} />
+              <Button variant="contained" color="primary" onClick={handleAddCategory}>add category</Button>
+            </Paper>
+          </Modal>
+
+          <Button onClick={handleEditCategory}>edit categories</Button>
+
+          <label style={{ textAlign: "left", fontStyle: "italic" }}>
+            Expectation
+          </label>
+          <input
+            name="expectation"
+            value={values.expectation}
+            onChange={handleInput}
+            style={{
+              marginBottom: ".3rem",
+              padding: "1rem",
+              borderRadius: "5px",
+              border: "1px solid lightblue",
+              outlineColor: "blue",
+            }}
+          />
+          <Button type="submit" variant="contained" color="primary">
+            {ideaId === "" ? "SAVE" : "Update"}
+          </Button>
         </form>
       </Paper>
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         {Object.keys(ideaObj).map((id, index) => {
           return (
-            <Paper key={id} style={{ width: "400px", margin: "2rem auto" }}>
-              <p>{index + 1}</p>
-              <Divider/>
-              <p>{ideaObj[id].title}</p>
-              <Divider/>
-              <p style={{wordWrap: 'break-word', padding:'1rem'}}>{ideaObj[id].longTitle}</p>
-              <Divider/>
-              <p>rating: {ideaObj[id].rating}</p>
-              <Divider/>
-              <p>{ideaObj[id].category}</p>
-              <Divider/>
-              <p>Created at: {ideaObj[id].timestamp}</p>
-              <Divider/>
+            <Paper
+              key={id}
+              style={{ width: "400px", margin: "2rem auto", padding: "1rem" }}
+            >
+              <Typography style={{ display: "flex" , marginBottom:"1rem"}}>{index + 1}.</Typography>
+              <Typography style={{ display: "flex",wordWrap: "break-word" }}>Title: {ideaObj[id].title}</Typography>
+              <Divider />
+              <Typography style={{ wordWrap: "break-word", display: "flex" }}>
+                Description: {ideaObj[id].longTitle}
+              </Typography>
+              <Divider />
+              <Typography style={{ display: "flex" }}>Rating: {ideaObj[id].rating}</Typography>
+              <Divider />
+              <Typography style={{ display: "flex", wordWrap: "break-word" }}>
+                Category: {ideaObj[id].category}
+              </Typography>
+              <Divider />
+              <Typography style={{ display: "flex", fontStyle: "italic",wordWrap: "break-word" }}>
+                Created at: {ideaObj[id].timestamp}
+              </Typography>
+              <Divider />
 
               <div
                 style={{
                   display: "flex",
                   justifyContent: "space-between",
                   alignItems: "center",
+                  marginTop: "1rem",
                 }}
               >
                 <Button onClick={() => setIdeaId(id)}>
-                  <a href="#" style={{ textDecoration: "none",}}>
+                  <a href="#" style={{ textDecoration: "none" }}>
                     <EditIcon color="primary" />
                   </a>
                 </Button>
